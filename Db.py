e2727740e3a1f4b1a8d09d29a4eb27bb56924570
Silvio Marx <136499496+silviomarx@users.noreply.github.com>
2023-06-21 21:39:21 +0200

    Fleshing out the Db class.

--- Db.py
+++ Db.py
@@ -1,62 +1,86 @@
 from database.initialize import initialize
 import sqlite3
 
-class Db():
+
+class Db:
 
     '''
-    Base Database Class for tracknut
-    that will perform all the operations
+    Base database class for tracknut
+    that will perform all the database operations.
     '''
 
     def __init__(self):
         self.connection = sqlite3.connect('database/food_data.db')
         self.cursor = self.connection.cursor()
+        self.fid = 0
+        self.mid = 0
         self.get_max_fid()
-
+        self.get_max_mid()
 
     def get_max_fid(self):
-        self.cursor.execute('SELECT MAX(ID) FROM food')
-        result = self.cursor.fetchone()[0]
-        if result is not None:
-            self.fid = int(result) + 1
+        try:
+            self.cursor.execute('SELECT MAX(ID) FROM food')
+            result = self.cursor.fetchone()[0]
+            if result is not None:
+                self.fid = int(result) + 1
 
-        else:
+            else:
+                self.fid = 0
+        except sqlite3.OperationalError:
             self.fid = 0
 
+    def get_max_mid(self):
+        try:
+            self.cursor.execute('SELECT MAX(ID) FROM meals')
+            result = self.cursor.fetchone()[0]
+            if result is not None:
+                self.mid = int(result) + 1
+
+            else:
+                self.mid = 0
+        except sqlite3.OperationalError:
+            self.mid = 0
 
     def initialize(self, file):
         initialize(file)
 
-    def init_foodtable(self, headers:list):
+    def init_food(self, headers: list):
         try:
-            self.fth = [header.replace('.', '').replace(' ', '').replace('-', '') for header in list(headers)]
-            self.fthstring = ', '.join(['\'' + header.replace('\'', '').replace('"', '') + '\'' for header in self.fth])
-            self.cursor.execute(f'CREATE TABLE food (\'ID\', {self.fthstring})')
+            fth = [header.replace('.', '').replace(' ', '').replace('-', '') for header in list(headers)]
+            fthstring = ', '.join(['\'' + header.replace('\'', '').replace('"', '') + '\'' for header in fth])
+            self.cursor.execute(f'CREATE TABLE food (\'ID\', {fthstring})')
         except sqlite3.OperationalError:
             raise sqlite3.OperationalError('Initialization already completed')
 
+    def init_meals(self):
+        try:
+            self.cursor.execute('CREATE TABLE meals (\'ID\',\'name\',\'ingredients\'')
+        except sqlite3.OperationalError:
+            raise sqlite3.OperationalError('Initialization already completed')
 
-    def insert_food(self, values:list):
-        self.ftv = [str(value) for value in list(values)]
-        self.ftvstring = ', '.join(['\'' + value.replace('\'', '').replace('"', '') + '\'' for value in self.ftv])
-        self.cursor.execute(f'INSERT INTO food VALUES(\'{self.fid}\',{self.ftvstring})')
+    def init_calendar(self):  # TODO
+        pass
+
+    def insert_food(self, values: list):
+        ftv = [str(value) for value in list(values)]
+        ftvstring = ', '.join(['\'' + value.replace('\'', '').replace('"', '') + '\'' for value in ftv])
+        self.cursor.execute(f'INSERT INTO food VALUES(\'{self.fid}\',{ftvstring})')
         self.connection.commit()
         self.fid += 1
 
-    def init_meals(self):
-        self.cursor.execute(f'CREATE TABLE meals (ID, meal')
+    def insert_meal(self, name, ingredients: dict):
+        self.cursor.execute(f'INSERT INTO meals VALUES(\'{self.mid}\',\'{name}\',\'{ingredients}\')')
+        self.connection.commit()
+        self.mid += 1
 
-    def init_calendar(self): # TODO
-        pass
+    def get_food(self, getid):
+        self.cursor.execute(f'SELECT * FROM food WHERE(ID = {getid})')
+        return self.cursor.fetchone()
 
+    def get_meal(self, getid):
+        self.cursor.execute(f'SELECT * FROM meals WHERE(ID = {getid})')
+        return self.cursor.fetchone()
 
-
-
-
-
-
-
-
-
-
-
+    def get_fdata(self, getid):
+        self.cursor.execute(f'SELECT * FROM fooddata WHERE(ID = {getid})')
+        return self.cursor.fetchone()
