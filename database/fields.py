f581808a843fd564c1c35442a706d91c54b6e952
Silvio Marx <136499496+silviomarx@users.noreply.github.com>
2023-06-29 15:40:25 +0200

    Initial version of the fields to be used in the program, that can be bound to the columns of the database

--- database/fields.py
+++ database/fields.py
@@ -7,31 +7,38 @@ class Fields:
             open('fields', 'xt').close()
             # Insert standard fields
             with open('fields', 'wt') as file:
-                file.write("""'Alpha Carotene', 'Beta Carotene', 'Beta Cryptoxanthin',
-                            'Carbohydrate', 'Cholesterol', 'Choline', 'Fiber',
-                            'Lutein and Zeaxanthin', 'Lycopene', 'Niacin', 'Protein',
-                            'Retinol', 'Riboflavin', 'Selenium', 'Sugar Total', 'Thiamin',
-                            'Water', 'Monosaturated Fat', 'Polysaturated Fat', 'Saturated Fat',
-                            'Total Lipid', 'Calcium', 'Copper', 'Iron', 'Magnesium', 'Phosphorus',
-                            'Potassium', 'Sodium', 'Zinc', 'Vitamin A - RAE', 'Vitamin B12', 'Vitamin B6',
-                            'Vitamin C', 'Vitamin E', 'Vitamin K'"""
-                           )
-
+                stdfields = [item + ', \n' for item in ['Alpha Carotene', 'Beta Carotene', 'Beta Cryptoxanthin',
+                                                        'Carbohydrate', 'Cholesterol', 'Choline', 'Fiber',
+                                                        'Lutein and Zeaxanthin', 'Lycopene', 'Niacin', 'Protein',
+                                                        'Retinol', 'Riboflavin', 'Selenium', 'Sugar Total', 'Thiamin',
+                                                        'Water', 'Monosaturated Fat', 'Polysaturated Fat',
+                                                        'Saturated Fat', 'Total Lipid', 'Calcium', 'Copper', 'Iron',
+                                                        'Magnesium', 'Phosphorus', 'Potassium', 'Sodium', 'Zinc',
+                                                        'Vitamin A - RAE', 'Vitamin B12', 'Vitamin B6',
+                                                        'Vitamin C', 'Vitamin E', 'Vitamin K']]
+                file.writelines(stdfields)
 
         except FileExistsError:
             pass
 
         with open('fields', 'rt') as file:
-            self.fields = list(file.read())
+            self.fields = list(file.read().split(', \n'))
 
     def add_item(self, item):
-        try:
-            with open('fields', 'at') as file:
-                file.write(f", '{item}'")
 
-        except: # TODO
-            raise
+        if item not in self.fields:
+            try:
+                with open('fields', 'at') as file:
+                    file.write(f'{item}, \n')
 
+                with open('fields', 'rt') as file:
+                    self.fields = list(file.read().split(', \n'))
 
+            except:  # TODO
+                raise
 
+        else:
+            raise ValueError('Item already exists in fields.')
 
+    def __str__(self):
+        return str(self.fields)
